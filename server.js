e161f678e30b568442af03dbb0161fa55d7a2ddb
Ananad Rao Gaikwad <123agaikwad@gmail.com>
2023-03-01 23:50:03 +0530

    new update

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors({
+    origin: true
+}));
 
 dotenv.config({ path: "./config.env" });
 const port = process.env.PORT;
